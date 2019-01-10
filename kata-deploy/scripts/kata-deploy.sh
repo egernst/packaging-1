77c25d8c8fe87a0d89bc8640605c97b0cfd2278c
Eric Ernst <eric.ernst@intel.com>
2019-01-10 09:33:26 -0800

    addressing review comments
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- kata-deploy/scripts/kata-deploy.sh
+++ kata-deploy/scripts/kata-deploy.sh
@@ -8,6 +8,14 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+
+# If we fail for any reason a message will be displayed
+die() {
+        msg="$*"
+        echo "ERROR: $msg" >&2
+        exit 1
+}
+
 function print_usage() {
 	echo "Usage: $0 [install/cleanup/reset]"
 }
@@ -15,15 +23,14 @@ function print_usage() {
 function get_container_runtime() {
 	local runtime=$(kubectl describe node $NODE_NAME)
 	if [ "$?" -ne 0 ]; then
-		echo invalid-node name
-		exit
+                die "invalid node name"
 	fi
 	echo "$runtime" | awk -F'[:]' '/Container Runtime Version/ {print $2}' | tr -d ' '
 }
 
 function install_artifacts() {
 	echo "copying kata artifacts onto host"
-	cp -R /opt/kata-artifacts/opt/kata/* /opt/kata/
+	cp -a /opt/kata-artifacts/opt/kata/* /opt/kata/
 	chmod +x /opt/kata/bin/*
 }
 
@@ -45,13 +52,20 @@ function configure_crio() {
 	# Configure crio to use Kata:
 	echo "Add Kata Containers as a supported runtime:"
 
+
 	# backup the CRIO.conf only if a backup doesn't already exist (don't override original)
-	if [ ! -f /etc/crio/crio.conf.bak ]; then
-		cp /etc/crio/crio.conf /etc/crio/crio.conf.bak
-	fi
-	echo -e "\n[crio.runtime.runtimes.kata-qemu]\nruntime_path = \"/opt/kata/bin/kata-qemu\"" >>/etc/crio/crio.conf
-	echo -e "\n[crio.runtime.runtimes.kata-fc]\nruntime_path = \"/opt/kata/bin/kata-fc\"" >>/etc/crio/crio.conf
-	sed -i 's|\(\[crio\.runtime\]\)|\1\nmanage_network_ns_lifecycle = true|' /etc/crio/crio.conf
+	conf_file="/etc/crio/crio.conf"
+	backup_conf_file="${conf_file}.bak"
+	cp -n "$conf_file" "$backup_conf_file"
+
+	cat <<EOT | tee -a "$conf_file"
+[crio.runtime.runtimes.kata-qemu]
+  runtime_path = "/opt/kata/bin/kata-qemu"
+[crio.runtime.runtimes.kata-fc]
+  runtime_path = "/opt/kata/bin/kata-fc"
+EOT
+
+	sed -i 's|\(\[crio\.runtime\]\)|\1\nmanage_network_ns_lifecycle = true|' "$conf_file"
 }
 
 function configure_containerd() {
@@ -64,6 +78,7 @@ function configure_containerd() {
 	fi
 	# TODO: While there isn't a default here anyway, it'd probably be best to
 	#  add sed magic to insert into appropriate location if config.toml already exists
+	# https://github.com/kata-containers/packaging/issues/307
 	cat <<EOT | tee /etc/containerd/config.toml
 [plugins]
     [plugins.cri.containerd]
@@ -112,6 +127,13 @@ function reset_runtime() {
 }
 
 function action() {
+
+	# script requires that user is root
+	euid=`id -u`
+	if [[ $euid -ne 0 ]]; then
+	   die  "This script must be run as root"
+	fi
+
 	runtime=$(get_container_runtime)
 
 	# only install / remove / update if we are dealing with CRIO or containerd
@@ -138,6 +160,8 @@ function action() {
 		esac
 	fi
 
+	#It is assumed this script will be called as a daemonset. As a result, do
+        # not return, otherwise the daemon will restart and rexecute the script
 	sleep infinity
 }
 
