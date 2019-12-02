302462297206c573bd5c70f917ebf4e7b50e5913
Eric Ernst <eric.ernst@intel.com>
2019-12-02 13:40:17 -0800

    testing: add workflows for testing kata-deploy
    1. A new action is introduced for testing kata-deploy on az.
    2. AKS based action moved to subdirectory
    3. Workflow introduced which leverages both of these actions.
    
    The workflow itself uses the following github action: xt0rted/slash-command-action
    
    The workflow will create a kata-deploy container image based off of the latest
    release, utilizing the latest released Kata artifacts off of master. It
    will then use both the AZ and AKS GitHub action.
    
    AZ was created in order to allow for customizing the snapshotter used
    (ie, so we can test Firecracker). In the future, it'll probably make
    sense to use CRIO instead of containerd here, to improve our test
    coverage against CRI implementations.
    
    Users with admin access on the repo can trigger this test by:
    /test kata-deploy
    
    Fixes: #845
    
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

---
+++ kata-deploy/actions/k3s/setup-k3s.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# Copyright (c) 2019 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+set -o errexit
+set -o pipefail
+set -o nounset
+
+LOCATION=${LOCATION:-eastus2}
+DNS_PREFIX=${DNS_PREFIX:-kata-deploy-${GITHUB_SHA:0:10}}
+
+function die() {
+	msg="$*"
+	echo "ERROR: $msg" >&2
+	exit 1
+}
+
+function destroy_k3s() {
+
+	az login --service-principal -u "$AZ_APPID" -p "$AZ_PASSWORD" --tenant "$AZ_TENANT_ID"
+	az group delete --name "$DNS_PREFIX" --yes --no-wait
+	az logout
+}
+
+function setup_k3s() {
+
+	[[ -z "$AZ_APPID" ]] && die "no Azure service principal ID provided"
+	[[ -z "$AZ_PASSWORD" ]] && die "no Azure service principal secret provided"
+	[[ -z "$AZ_SUBSCRIPTION_ID" ]] && die "no Azure subscription ID provided"
+	[[ -z "$AZ_TENANT_ID" ]] && die "no Azure tenant ID provided"
+
+	# Create the virtual machine:
+	az login --service-principal -u "$AZ_APPID" -p "$AZ_PASSWORD" --tenant "$AZ_TENANT_ID"
+
+	# create resource group
+	az group create --name ${DNS_PREFIX} --location ${LOCATION}
+
+	# setup security groups for inbound:
+	az network nsg create --resource-group ${DNS_PREFIX} --name k3s-nsg
+	az network nsg rule create --name ssh-access --nsg-name k3s-nsg --resource-group ${DNS_PREFIX} --priority 100 --access Allow --source-address-prefixes '*' --source-port-ranges '*' --destination-address-prefixes '*' --destination-port-ranges 22 --protocol Tcp
+	az network nsg rule create --name access-api-server --nsg-name k3s-nsg --resource-group ${DNS_PREFIX} --priority 101 --access Allow --source-address-prefixes '*' --source-port-ranges '*' --destination-address-prefixes '*' --destination-port-ranges 6443 --protocol Tcp
+
+	# create key for the nodes. In GHActions, we cannot modify default keys (~/.ssh/), so we need to generate
+	# our own at an acceptable location
+	KEY_PATH=/tmp/id_rsa
+	ssh-keygen -f ${KEY_PATH} -t rsa -N ''
+
+	result=$(az vm create \
+		--resource-group ${DNS_PREFIX} \
+		--size Standard_D4S_v3 \
+		--name ${DNS_PREFIX}-control \
+		--image UbuntuLTS \
+		--nsg k3s-nsg \
+		--admin-username kata \
+		--ssh-key-values ${KEY_PATH}.pub )
+	controlIP=$(echo $result | jq -r '.publicIpAddress')
+
+	result=$(az vm create \
+		--resource-group ${DNS_PREFIX} \
+		--size Standard_D4S_v3 \
+		--name ${DNS_PREFIX}-worker \
+		--image UbuntuLTS \
+		--nsg k3s-nsg \
+		--admin-username kata \
+		--ssh-key-values ${KEY_PATH}.pub )
+	workerIP=$(echo $result | jq -r '.publicIpAddress')
+
+	set -x
+
+	echo control, worker: ${controlIP} ${workerIP}
+
+	# Setup K3s on control/worker:
+	curl -sLS https://raw.githubusercontent.com/alexellis/k3sup/master/get.sh | sh
+	k3sup  install --ip $controlIP --user kata --ssh-key ${KEY_PATH}
+	k3sup join --ip ${workerIP} --server-ip ${controlIP} --user kata --ssh-key ${KEY_PATH}
+
+	export KUBECONFIG=$PWD/kubeconfig
+}
--- kata-deploy/actions/k3s/test-kata.sh
+++ kata-deploy/actions/k3s/test-kata.sh
@@ -17,8 +17,8 @@ function die() {
 
 function waitForProcess() {
     wait_time="$1"
-    sleep_time="$2"
-    cmd="$3"
+    cmd="$2"
+    sleep_time=5
     while [ "$wait_time" -gt 0 ]; do
         if eval "$cmd"; then
             return 0
@@ -28,64 +28,80 @@ function waitForProcess() {
             wait_time=$((wait_time-sleep_time))
         fi
     done
-    return 1
+    die "wait for $cmd timed out"
 }
 
 # waitForLabelRemoval will wait for the kata-runtime labels to removed until a given
 # timeout expires
 function waitForLabelRemoval() {
     wait_time="$1"
-    sleep_time="$2"
+    sleep_time=5
 
+    echo "waiting for kata-runtime label to be removed"
     while [[ "$wait_time" -gt 0 ]]; do
         # if a node is found which matches node-select, the output will include a column for node name,
         # NAME. Let's look for that 
-        if [[ -z $(kubectl get nodes --selector katacontainers.io/kata-runtime | grep NAME) ]]
+        if [[ -z $(kubectl get --request-timeout='30s' nodes --selector katacontainers.io/kata-runtime | grep NAME) ]]
         then
             return 0
         else
-            echo "waiting for kata-runtime label to be removed"
             sleep "$sleep_time"
             wait_time=$((wait_time-sleep_time))
         fi
     done
 
-    echo $(kubectl get pods,nodes --all-namespaces --show-labels)
-
-    echo "failed to cleanup"
-    return 1
+    echo $(kubectl get --request-timeout='30s' nodes --show-labels)
+    die "failed to cleanup"
 }
 
+function myKubectlWait() {
+  wait_time="$1"
+  wait_cmd="$2"
+  sleep_time=5
+
+  echo "waiting for $2"
+  while [[ "$wait_time" -gt 0 ]]; do
+    if [[   $(kubectl wait --timeout=0 $wait_cmd) ]]; then
+      echo "condition met"
+      return 0
+    else
+      sleep "$sleep_time"
+      wait_time=$((wait_time-sleep_time))
+    fi 
+  done
+
+  die "wait for $2 failed"
+}
 
 function run_test() {
-    PKG_SHA=$1
-    YAMLPATH="https://raw.githubusercontent.com/kata-containers/packaging/$PKG_SHA/kata-deploy"
+    YAMLPATH="./kata-deploy"
+
     echo "verify connectivity with a pod using Kata"
 
     deployment=""
     busybox_pod="test-nginx"
     busybox_image="busybox"
-    cmd="kubectl get pods | grep $busybox_pod | grep Completed"
+    cmd="kubectl --request-timeout='30s' get pods | grep $busybox_pod | grep Completed"
     wait_time=120
     sleep_time=3
 
     configurations=("nginx-deployment-qemu" "nginx-deployment-qemu-virtiofs")
     for deployment in "${configurations[@]}"; do
-        # start the kata pod:
-        kubectl apply -f "$YAMLPATH/examples/${deployment}.yaml"
+      # start the kata pod:
+      kubectl apply -f "$YAMLPATH/examples/${deployment}.yaml"
 
       # in case the control plane is slow, give it a few seconds to accept the yaml, otherwise
       # our 'wait' for deployment status will fail to find the deployment at all
       sleep 3 
 
-      kubectl wait --timeout=5m --for=condition=Available deployment/${deployment}
+      myKubectlWait 600 "--for=condition=Available deployment/${deployment}"
       kubectl expose deployment/${deployment}
 
       # test pod connectivity:
       kubectl run $busybox_pod --restart=Never --image="$busybox_image" -- wget --timeout=5 "$deployment"
-      waitForProcess "$wait_time" "$sleep_time" "$cmd"
-      kubectl logs "$busybox_pod" | grep "index.html"
-      kubectl describe pod "$busybox_pod"
+      waitForProcess "$wait_time" "$cmd"
+      kubectl logs --request-timeout='30s' "$busybox_pod" | grep "index.html"
+      kubectl describe --request-timeout='30s'  pod "$busybox_pod"
 
       # cleanup:
       kubectl delete deployment "$deployment"
@@ -98,33 +114,32 @@ function run_test() {
 function test_kata() {
     set -x
 
+    echo $(ls)
+    echo $(pwd)
+
     [[ -z "$PKG_SHA" ]] && die "no PKG_SHA provided"
     echo "$PKG_SHA"
 
     #kubectl all the things
-    kubectl get pods,nodes --all-namespaces
+    kubectl get --request-timeout='30s' pods,nodes --all-namespaces
 
-    YAMLPATH="https://raw.githubusercontent.com/kata-containers/packaging/$PKG_SHA/kata-deploy"
-
+    YAMLPATH="./kata-deploy"
     kubectl apply -f "$YAMLPATH/kata-rbac/base/kata-rbac.yaml"
 
     # apply runtime classes:
     kubectl apply -f "$YAMLPATH/k8s-1.14/kata-qemu-runtimeClass.yaml"
     kubectl apply -f "$YAMLPATH/k8s-1.14/kata-qemu-virtiofs-runtimeClass.yaml"
 
-    kubectl get runtimeclasses
-
-    curl -LO "$YAMLPATH/kata-deploy/base/kata-deploy.yaml"
-    curl -LO "$YAMLPATH/kata-cleanup/base/kata-cleanup.yaml"
+    kubectl get --request-timeout='30s' runtimeclasses
 
     # update deployment daemonset to utilize the container under test:
-    sed -i "s#katadocker/kata-deploy#katadocker/kata-deploy-ci:${PKG_SHA}#g" kata-deploy.yaml
-    sed -i "s#katadocker/kata-deploy#katadocker/kata-deploy-ci:${PKG_SHA}#g" kata-cleanup.yaml
+    sed -i "s#katadocker/kata-deploy#katadocker/kata-deploy-ci:${PKG_SHA}#g" $YAMLPATH/kata-deploy/base/kata-deploy.yaml
+    sed -i "s#katadocker/kata-deploy#katadocker/kata-deploy-ci:${PKG_SHA}#g" $YAMLPATH/kata-cleanup/base/kata-cleanup.yaml
 
-    cat kata-deploy.yaml
+    cat $YAMLPATH/kata-deploy/base/kata-deploy.yaml
 
     # deploy kata:
-    kubectl apply -f kata-deploy.yaml
+    kubectl apply -k $YAMLPATH/kata-deploy/overlays/k3s
 
     # in case the control plane is slow, give it a few seconds to accept the yaml, otherwise
     # our 'wait' for deployment status will fail to find the deployment at all. If it can't persist
@@ -132,31 +147,41 @@ function test_kata() {
     sleep 30
 
     # wait for kata-deploy to be up
-    kubectl -n kube-system wait --timeout=10m --for=condition=Ready -l name=kata-deploy pod
+    myKubectlWait 600 "-n kube-system --for=condition=Ready -l name=kata-deploy pod"
 
     # show running pods, and labels of nodes
-    kubectl get pods,nodes --all-namespaces --show-labels
+    kubectl get --request-timeout='30s' pods,nodes --all-namespaces --show-labels
 
-    run_test $PKG_SHA
+    run_test
 
-    kubectl get pods,nodes --show-labels
+    kubectl get --request-timeout='30s' pods,nodes --show-labels
 
     # Remove Kata
-    kubectl delete -f kata-deploy.yaml
-    kubectl -n kube-system wait --timeout=10m --for=delete -l name=kata-deploy pod
+    kubectl delete -k $YAMLPATH/kata-deploy/overlays/k3s
 
-    kubectl get pods,nodes --show-labels
+    # wait for kata-deploy pod to be deleted; waiting for 10 minutes 
+    #kubectl -n kube-system wait --timeout=10m --for=delete -l name=kata-deploy pod
+    start=$(date +%s); now=$start
+    echo "wait for kata-deploy deletion"
+    until [[ ! $(kubectl get --ignore-not-found=true daemonset -n kube-system kata-deploy 2>&1) ]]; do 
+      sleep 2;
+      now=$(date +%s)
+      if [ $((now-start)) -gt 600 ]; then
+        die "timeout waiting for kata-deploy to delete"
+      fi 
+    done
 
-    kubectl apply -f kata-cleanup.yaml
+    kubectl get --request-timeout='30s' pods,nodes --show-labels
+
+    kubectl apply -f $YAMLPATH/kata-cleanup/base/kata-cleanup.yaml
 
     # The cleanup daemonset will run a single time, since it will clear the node-label. Thus, its difficult to
     # check the daemonset's status for completion. instead, let's wait until the kata-runtime labels are removed
     # from all of the worker nodes. If this doesn't happen after 2 minutes, let's fail
-    timeout=20
-    sleeptime=6
-    waitForLabelRemoval $timeout $sleeptime
+    timeout=120
+    waitForLabelRemoval $timeout
 
-    kubectl delete -f kata-cleanup.yaml
+    kubectl delete -f $YAMLPATH/kata-cleanup/base/kata-cleanup.yaml
 
     rm kata-cleanup.yaml
     rm kata-deploy.yaml
